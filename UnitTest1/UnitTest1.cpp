c532b6c5194ec38e036bd0173857430c107551fb
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-10-31 21:18:30 +0200

    UnitTest

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 5.3/Lab 5.3.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = f(2);
+			Assert::AreEqual(t, 3.87163, 0.00001);
+		}
+	};
+}
